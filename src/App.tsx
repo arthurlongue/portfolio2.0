02949b814d4be718a86821608d7c9a9af00589a3
Arthur Longue <arthurlongue@hotmail.com>
2024-02-14 14:44:39 -0300

    background fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Home from "./pages/Home/Home";
 function App() {
   return (
     <>
-      <body className="bg-[url('assets/matrixbg.jpg')] bg-contain"></body>
+      <body className="bg-[url('/assets/matrixbg.jpg')] bg-contain"></body>
       <Home></Home>
     </>
   );
